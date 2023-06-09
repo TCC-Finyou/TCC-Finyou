ccaad4b248913e116e41a588f0f47faccbaf3565
Vinícius Gonçalves <vinicius.devlp@gmail.com>
2023-06-08 21:58:07 -0300

    atualização do nome da variável port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const router = require('./app/routes/router');
 const app = express();
-const port = process.env.PORT_OUR
+const port = process.env.PORT
 
 app.use(express.static(path.join(__dirname, "app", "public")));
 
