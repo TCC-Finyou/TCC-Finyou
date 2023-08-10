34840ba6db2c18cf3683e811f148f3782a7319f5
Vinícius Gonçalves <vinicius.devlp@gmail.com>
2023-08-09 22:52:43 -0300

    pagamentos apenas dentro da Finyou

--- app/controllers/info-pages/assinatura/assinaturaPortalControllerCreate.js
+++ app/controllers/info-pages/assinatura/assinaturaPortalControllerCreate.js
@@ -10,7 +10,7 @@ class AssinaturaPortalController {
 
         const session = await stripe.billingPortal.sessions.create({
             customer: user.customer_id,
-            return_url: "https://finyou.up.railway.app/pacotes",
+            return_url: "https://finyou.up.railway.app/perfil",
         });
 
         res.redirect(session.url);
